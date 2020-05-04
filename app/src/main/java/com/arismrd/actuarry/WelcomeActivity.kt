80fa93286baeeadc54c6b9e7eb6ec290f632e5d3
Ari Sumardi <arismrd13@gmail.com>
2020-05-05 04:22:03 +0700

    Add navigation bottom

--- app/src/main/java/com/arismrd/actuarry/WelcomeActivity.kt
+++ app/src/main/java/com/arismrd/actuarry/WelcomeActivity.kt
@@ -4,7 +4,9 @@ import PreferenceHelper
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class WelcomeActivity : AppCompatActivity() {
     /**
@@ -13,21 +15,40 @@ class WelcomeActivity : AppCompatActivity() {
      * KELAS : IF-4
      * TUGAS UTS
      */
+    private fun setupNavigation() {
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    Toast.makeText(this, "Home selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.navigation_daily -> {
+                    Toast.makeText(this, "Daily selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.navigation_galery -> {
+                    Toast.makeText(this, "Galery selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.navigation_video -> {
+                    Toast.makeText(this, "Video selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.navigation_profile -> {
+                    Toast.makeText(this, "Profile selected", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> true
+            }
+        }
+    }
 
-    private var btn: Button? = null
     private var preferenceHelper: PreferenceHelper? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-
-        btn = findViewById(R.id.btn) as Button
         preferenceHelper = PreferenceHelper(this)
-
-        btn!!.setOnClickListener {
-            preferenceHelper!!.putIntro("")
-            onBackPressed()
-        }
-
     }
 }
